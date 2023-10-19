d59b243d9e7e96f31733a330790518e070dd46b8
a1ex.solovyov <a1ex.solovyov10@gmail.com>
2023-10-19 17:52:02 +0300

    03.01_Fixed

--- cppm-homework-03.01/main.cpp
+++ cppm-homework-03.01/main.cpp
@@ -10,11 +10,11 @@
 
 class Calculator
 {
-  public:
-    double num1;
-    double num2;
+  private:
+     double num1;
+     double num2;
     
-//private:
+  public:
     double add ()
     {
         double sum = num1 + num2;
@@ -45,19 +45,43 @@ class Calculator
         double divide_2 = num2/num1;
         return divide_2;
     }
+    
     bool set_num1 (double num1)
     {
+        std::cout << "Please enter Num1: ";
+        std::cin >> num1;
+        while (num1 == 0)
+        {
+            std::cout << "Wrong number!" << '\n';
+            std::cout << "Please enter Num1: ";
+            std::cin >> num1;
+        };
         if (num1 != 0){
-            this-> num1 = num1;
-            return true;
-        } else return false;
+                this-> num1 = num1;
+                return true;
+            } else {
+                return false;
+            }
     }
+    
     bool set_num2 (double num2)
-    {
-        if (num2 != 0){
+   {
+        std::cout << "Please enter Num2: ";
+        std::cin >> num2;
+        while (num2 == 0)
+        {
+            std::cout << "Wrong number!" << '\n';
+            std::cout << "Please enter Num2: ";
+            std::cin >> num2;
+        };
+        if (num2 != 0)
+        {
             this-> num2 = num2;
             return true;
-        } else return false;
+        } else
+        {
+            return false;
+        }
     }
 };
 
@@ -68,37 +92,14 @@ class Calculator
 
 
 int main(int argc, const char * argv[]) {
+    
+   
    
     Calculator calculator;
     
-    for  ( ; ; )
-    {
-        std::cout << "Please enter num1: ";
-        std::cin  >> calculator.num1;
+    calculator.set_num1(0);
         
-        
-        for (calculator.set_num1(0);calculator.num1 == 0;)
-        {
-            std::cout << "Wrong number!" << '\n';
-            std::cout << "Please enter num1: ";
-            std::cin  >> calculator.num1;
-            
-        };
-        
-        
-        std::cout << "Please enter num2: ";
-        std::cin  >> calculator.num2;
-      
-        
-        for (calculator.set_num2(0);calculator.num2 == 0;)
-        {
-            std::cout << "Wrong number!" << '\n';
-            std::cout << "Please enter num2: ";
-            std::cin  >> calculator.num2;
-         
-        };
-       
-        
+    calculator.set_num2(0);
         
         std::cout << "num1 + num 2 = " << calculator.add() << '\n';
         std::cout << "num1 - num 2 = " << calculator.subtract_2_1() << '\n';
@@ -106,7 +107,6 @@ int main(int argc, const char * argv[]) {
         std::cout << "num1 * num 2 = " << calculator.multyply() << '\n';
         std::cout << "num1 / num 2 = " << calculator.divide_1_2() << '\n';
         std::cout << "num2 / num 1 = " << calculator.divide_2_1() << '\n';
-    }
     
     return 0;
 }
