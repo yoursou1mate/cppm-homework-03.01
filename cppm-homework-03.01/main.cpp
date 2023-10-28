ab12ac3f6aa95e59adc9923fee4baaf5bfe4e0a1
a1ex.solovyov <a1ex.solovyov10@gmail.com>
2023-10-28 17:11:05 +0300

    3.01_Fixed_2

--- cppm-homework-03.01/main.cpp
+++ cppm-homework-03.01/main.cpp
@@ -48,14 +48,6 @@ class Calculator
     
     bool set_num1 (double num1)
     {
-        std::cout << "Please enter Num1: ";
-        std::cin >> num1;
-        while (num1 == 0)
-        {
-            std::cout << "Wrong number!" << '\n';
-            std::cout << "Please enter Num1: ";
-            std::cin >> num1;
-        };
         if (num1 != 0){
                 this-> num1 = num1;
                 return true;
@@ -66,14 +58,6 @@ class Calculator
     
     bool set_num2 (double num2)
    {
-        std::cout << "Please enter Num2: ";
-        std::cin >> num2;
-        while (num2 == 0)
-        {
-            std::cout << "Wrong number!" << '\n';
-            std::cout << "Please enter Num2: ";
-            std::cin >> num2;
-        };
         if (num2 != 0)
         {
             this-> num2 = num2;
@@ -94,12 +78,31 @@ class Calculator
 int main(int argc, const char * argv[]) {
     
    
-   
+    int i = 0;
+    int j = 0;
     Calculator calculator;
     
-    calculator.set_num1(0);
+    for (;;)
+    {
+        std::cout << "Please enter Num1: ";
+        std::cin >> i;
+        while (i == 0)
+        {
+            std::cout << "Wrong number!" << '\n';
+            std::cout << "Please enter Num1: ";
+            std::cin >> i;
+        };
+        calculator.set_num1(i);
         
-    calculator.set_num2(0);
+        std::cout << "Please enter Num2: ";
+        std::cin >> j;
+        while (j == 0)
+        {
+            std::cout << "Wrong number!" << '\n';
+            std::cout << "Please enter Num2: ";
+            std::cin >> j;
+        };
+        calculator.set_num2(j);
         
         std::cout << "num1 + num 2 = " << calculator.add() << '\n';
         std::cout << "num1 - num 2 = " << calculator.subtract_2_1() << '\n';
@@ -107,6 +110,7 @@ int main(int argc, const char * argv[]) {
         std::cout << "num1 * num 2 = " << calculator.multyply() << '\n';
         std::cout << "num1 / num 2 = " << calculator.divide_1_2() << '\n';
         std::cout << "num2 / num 1 = " << calculator.divide_2_1() << '\n';
+    }
     
     return 0;
 }
